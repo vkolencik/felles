45e3cee54861f90db81a8dc0e52c949163b7134e
Vojtech Kolencik <kolencik@gmail.com>
2017-11-20 00:49:13 +0100

    added logging

--- index.js
+++ index.js
@@ -200,6 +200,7 @@ function getSelectionMessage(title, options, createPayload) {
 }
 
 function callSendAPI(sender_psid, response) {
+    console.log('Sending message: ' + response);
     // Construct the message body
     let request_body = {
         "recipient": {
