065d71a030ccbe6d32833695721eea2c9682ec98
Vojtech Kolencik <kolencik@gmail.com>
2017-11-20 01:22:10 +0100

    added missing parameter

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ function handlePostback(sender_psid, received_postback) {
                 break;
             case 'type':
                 db.saveUserEducationType(sender_psid, responseValue);
-                callSendAPI({"text": "Super, jakmile se něco šustne tak se ozvu ;)"});
+                callSendAPI(sender_psid, {"text": "Super, jakmile se něco šustne tak se ozvu ;)"});
                 break;
         }
     }
