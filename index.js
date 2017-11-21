69aa74c2ddc24c841cf20edca9ad948a017e40db
Vojtech Kolencik <kolencik@gmail.com>
2017-11-21 07:33:41 +0100

    test

--- index.js
+++ index.js
@@ -247,31 +247,37 @@ function setUpBotProfile() {
             "locale":"default",
             "text":"Ahoj!"
         }],
-        "get_started": { "payload" : "getStartedPostback" }/*,
+        "get_started": { "payload" : "getStartedPostback" },
         "persistent_menu": [
             {
                 "locale": "default",
                 "composer_input_disabled": true,
                 "call_to_actions": [
+
                     {
-                        "title": "Znovu zadat údaje",
+                        "title": "XXX",
                         "type": "postback",
-                        "payload": "getStartedPostback"
-                    },
-                    {
-                        "type": "web_url",
-                        "title": "Stránka se změnami rozvrhu",
-                        "url": CLASS_DATA_URL,
-                        "webview_height_ratio": "full"
-                    },
-                    {
-                        "title": "Odhlásit se",
-                        "type": "postback",
-                        "payload": "unsubscribe"
+                        "payload": "XXX"
                     }
+                    // {
+                    //     "title": "Znovu zadat údaje",
+                    //     "type": "postback",
+                    //     "payload": "getStartedPostback"
+                    // },
+                    // {
+                    //     "type": "web_url",
+                    //     "title": "Stránka se změnami rozvrhu",
+                    //     "url": CLASS_DATA_URL,
+                    //     "webview_height_ratio": "full"
+                    // },
+                    // {
+                    //     "title": "Odhlásit se",
+                    //     "type": "postback",
+                    //     "payload": "unsubscribe"
+                    // }
                 ]
             }
-        ]*/
+        ]
     };
 
     request({
